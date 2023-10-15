2788d18e9d7d6777b2e662d34d21fb2584c4b4cf
matdenkas <mateokastl@gmail.com>
2023-10-15 15:43:48 -0500

    Documentation

--- server.js
+++ server.js
@@ -2,21 +2,28 @@ const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path'); 
 
+//Load in .env file.
 dotenv.config();
 
+//Construct a new webapp, this is our server
 const app = express();
+
+//Read in what port to use from the env
 const port = process.env.PORT;
 
+//Declare the dist directory as our static dir, and host it. Anything that hits
+// the / endpoint will get the dist dir back
 const publicPath = path.join(__dirname, 'dist');
 app.use(express.static(publicPath));
 
 
 
-
+//THis is an example of a get request
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname, '/dist/index.html'));
 // });
 
+//Start are server on the port we selected.
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
