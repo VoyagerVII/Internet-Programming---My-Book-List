3deabb1811d82d23fe414ec3c73dc52a1db5b02c
matdenkas <mateokastl@gmail.com>
2023-10-15 15:55:24 -0500

    Documentation

--- Back/server.js
+++ Back/server.js
@@ -1,3 +1,17 @@
+// -*- coding: utf-8 -*-
+//server.js
+//----------------------------------
+// Created By: Matthew Kastl
+// Created Date: 1/15/2023
+// version 1.0
+//----------------------------------
+// """This file is an express webserver
+// that serves the public direcotry under root.
+//  """ 
+//----------------------------------
+// 
+//
+//Imports
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path'); 
